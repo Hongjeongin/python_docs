cdc611d33b9f2d7abeeab037b04648e8e78ac6f5
Hongjeongin <64721629+Hongjeongin@users.noreply.github.com>
2022-06-20 18:17:44 +0900

    First python commit!!
    first commit

---
+++ numbers.py
@@ -0,0 +1,26 @@
+# My first python project
+# This file is play with numbers
+
+print(2 + 2) # result = 4
+
+print(50 - 5 * 6) # result = 20
+
+print((50 - 5 * 6) / 4) # result = 5.0
+
+print(8 / 5) # result = 1.6 >> division always retuns a floating point number
+
+print(17 / 3) # classic division retuns a float
+
+print(17 // 3) # result = 5
+
+print(17 % 3) # result = 2
+
+print(5 * 3 + 2) # result = 17
+
+print(5 ** 2) # result = 25 >> 5 squared
+
+print(2 ** 7) # reulst = 128 >> 2 to the power of 7
+
+width = 20
+height = 5 * 11
+print(width * height) # reulst = 1100
---
+++ syntax_if.py
@@ -0,0 +1,16 @@
+five = 5
+four = 4
+"""
+if five > four:
+    print('당연하지')
+    print('내가 좀 똑똑해')
+    five == four:
+    print('그건 틀린 거야')
+    print('다시 생각해봐')
+else:
+    print('숫자를 잘못 배웠구나')
+"""
+
+from datetime import datetime
+hour = datetime.now().hour
+print(hour)
